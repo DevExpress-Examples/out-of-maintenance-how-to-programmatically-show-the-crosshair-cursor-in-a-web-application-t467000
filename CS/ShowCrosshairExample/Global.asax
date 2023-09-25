ff2d8a8ac5e806627a07f39db43b773335f882e3
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-25 19:53:21 +0400

    Change example status to out of maintenance

--- CS/ShowCrosshairExample/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShowCrosshairExample.Global" Language="C#" %>
--- CS/ShowCrosshairExample/MainPageScripts.js
+++
@@ -1,7 +0,0 @@
-﻿function OnChartObjectSelectionChanged(s, e) {
-    var x = e.absoluteX - e.htmlElement.offsetLeft;
-    var y = e.absoluteY - e.htmlElement.offsetTop;
-    var xyDiagram = s.GetChart().diagram;
-    xyDiagram.ShowCrosshair(x, y);
-    e.processOnServer = false;
-}
